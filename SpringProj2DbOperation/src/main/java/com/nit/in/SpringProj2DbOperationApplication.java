a70f6d696ee6318656e0f7a5f4f98fec59099ca4
TL <TL@gmail.com>
2021-11-26 15:49:40 +0530

    intial Import By Tl

---
+++ SpringProj2DbOperation/src/main/java/com/nit/in/SpringProj2DbOperationApplication.java
@@ -0,0 +1,13 @@
+package com.nit.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProj2DbOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProj2DbOperationApplication.class, args);
+	}
+
+}
---
+++ SpringProj2DbOperation/src/test/java/com/nit/in/SpringProj2DbOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProj2DbOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
